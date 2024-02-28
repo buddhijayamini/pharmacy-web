5d378bdb76b783a7e854695c341da8adaf63049e
Buddhini SSE <buddhigeethen@gmail.com>
2024-02-28 13:13:04 +0530

    migrations and seeders added

--- database/migrations/2024_02_28_060728_create_customers_table.php
+++ database/migrations/2024_02_28_060728_create_customers_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('role_id');
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->string('name',255);
+            $table->string('address',255)->nullable();
+            $table->string('mobile',45)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customers');
     }
 };
