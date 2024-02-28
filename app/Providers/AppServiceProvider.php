cc5fdd84868353bb3c68172e5c7ac4fe31ac904c
Buddhini SSE <buddhigeethen@gmail.com>
2024-02-28 13:11:42 +0530

    middleware added for user roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\MedicationInterface;
+use App\Repositories\MedicationRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            MedicationInterface::class,
+            MedicationRepository::class
+        );
     }
 
     /**
